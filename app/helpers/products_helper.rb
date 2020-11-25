0b8af9083a32b425e5dea29980ce62ba2e8f9f0a
Allan <allan@ientz.me>
2020-11-25 10:03:39 -0700

    image magick server

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,18 +1,16 @@
 module ProductsHelper
 
 def large image
-    # image.variant(resize: 'x600>').processed
-    image
+    image.variant(resize: 'x600>').processed
 end
 
 def card image
-    # image.variant(combine_options: { resize: '300x300^', extent: '300x300', gravity: 'center' }).processed
-    image
+    image.variant(combine_options: { resize: '300x300^', extent: '300x300', gravity: 'center' }).processed
 end
 
 
 def thumb image
-    image
+    image.variant(combine_options: { resize: '150x150^', extent: '150x150', gravity: 'center' }).processed
 end
 
 def tiny image
