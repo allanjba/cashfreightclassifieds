fde9906fbdfebbc30705f43db554e92745e04bee
Allan <allan@ientz.me>
2021-04-24 11:14:05 -0600

    create controller

---
+++ app/controllers/users_dashboard_controler.rb
@@ -0,0 +1,38 @@
+class UsersDashboardController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        
+    end
+
+    def my_listings
+
+    end
+
+    def my_bids
+
+    end
+
+    def settings
+
+    end
+
+    def my_account
+
+    end
+
+    def payments
+        
+    end
+
+    def my_transactions
+
+    end
+
+
+
+
+
+
+
+end
