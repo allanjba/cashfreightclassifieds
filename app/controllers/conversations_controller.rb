6d90547979dfa02b9986efdbc10552aa97590711
Allan <allan@ientz.me>
2021-06-04 11:50:19 -0600

    fix svg on dashboard nav & new message template

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -33,7 +33,7 @@ class ConversationsController < ApplicationController
 
     def create
         recipient = User.find(params[:user_id])
-        receipt = current_user.send_message(recipient, params[:body], params[:subject])  
+        receipt = current_user.send_message(recipient, params[:body], "no-subject")  
         redirect_to conversation_path(receipt.conversation)
         # deliver methods
         MessageNotification.with({conversation: receipt.conversation.id, user: current_user}).deliver_later(from(receipt.conversation, current_user))
